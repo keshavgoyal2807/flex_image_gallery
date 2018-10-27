157ef064b9d75ec9f251bfa4b80489973aa8c915
KESHAV GOYAL <keshavgoyal2014@gmail.com>
2018-10-27 19:17:56 +0530

    added files for this project

---
+++ script.js
@@ -0,0 +1,9 @@
+var a=document.querySelectorAll('.main1');
+a.forEach(b => b.addEventListener('click',toogletext));
+a.forEach(b => b.addEventListener('click',tooglemenu));
+function toogletext(){
+    this.classList.toggle('active');
+}
+function tooglemenu(){
+    this.classList.toggle('open')
+}
